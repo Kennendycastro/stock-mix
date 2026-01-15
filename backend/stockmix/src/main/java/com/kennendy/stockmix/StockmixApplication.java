622b62c4c822236ea0151433520aad8dbe7fb7c5
Kennendycastro <kennendycastro3@gmail.com>
2026-01-15 09:50:00 -0300

    feat: Cria o back-end em Spring boot

---
+++ backend/stockmix/src/main/java/com/kennendy/stockmix/StockmixApplication.java
@@ -0,0 +1,13 @@
+package com.kennendy.stockmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmixApplication.class, args);
+	}
+
+}
