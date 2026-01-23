d6fa4ced658fb12b5682a56bcb9489691c9df161
Kennendycastro <kennendycastro3@gmail.com>
2026-01-23 09:23:39 -0300

    Retira importações não usadas

--- backend/stockmix/src/main/java/com/kennendy/stockmix/prdutos/ProdutosController.java
+++ backend/stockmix/src/main/java/com/kennendy/stockmix/prdutos/ProdutosController.java
@@ -2,8 +2,6 @@ package com.kennendy.stockmix.prdutos;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -48,7 +45,7 @@ public class ProdutosController {
 
     @PutMapping("/atualizar/{id}")
     public ProdutosModel atualizarProduto(@PathVariable Long id, @RequestBody ProdutosModel produtoatualizado) {
-        
+
         return produtosService.atualizarProduto(id, produtoatualizado);
     }
 
