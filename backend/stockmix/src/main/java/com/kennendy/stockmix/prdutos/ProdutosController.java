1c61e8be090a720a70a9c6bef0d1e9c824c7ccf9
Kennendycastro <kennendycastro3@gmail.com>
2026-02-01 20:25:59 -0300

    refactor: Refatora o controller para adicionar o ResponseEntity para melhor visualização da aplicação. (fixes: #11)

--- backend/stockmix/src/main/java/com/kennendy/stockmix/prdutos/ProdutosController.java
+++ backend/stockmix/src/main/java/com/kennendy/stockmix/prdutos/ProdutosController.java
@@ -1,6 +1,9 @@
 package com.kennendy.stockmix.prdutos;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +26,11 @@ public class ProdutosController {
     // C - CREATE
 
     @PostMapping("/cadastrar")
-    public ProdutosDTO cadastrarProduto(@RequestBody ProdutosDTO produto) {
+    public ResponseEntity<String> cadastrarProduto(@RequestBody ProdutosDTO produto) {
 
-        return produtosService.cadastrarProduto(produto);
+        ProdutosDTO novoProduto =produtosService.cadastrarProduto(produto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body("O prduto foi criado. Nome: "+novoProduto.getNome() +", ID: "+novoProduto.getIdProduto());
     }
 
     // R - READ
@@ -36,23 +41,47 @@ public class ProdutosController {
     }
 
     @GetMapping("/listar/{id}")
-    public ProdutosDTO listarProdutosId(@PathVariable Long id) {
-        return produtosService.listarProdutosId(id);
+    public ResponseEntity<?> listarProdutosId(@PathVariable Long id) {
+        ProdutosDTO produtoId = produtosService.listarProdutosId(id);
+
+        if (produtoId != null) {
+
+            return ResponseEntity.ok(produtoId);
+                 
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("Produto de ID: "+id+ " não encontrado");
     }
     
     // U -UPDATE
 
     @PutMapping("/atualizar/{id}")
-    public ProdutosDTO atualizarProduto(@PathVariable Long id, @RequestBody ProdutosDTO produtoatualizado) {
+    public ResponseEntity<?> atualizarProduto(@PathVariable Long id, @RequestBody ProdutosDTO produtoatualizado) {
 
-        return produtosService.atualizarProduto(id, produtoatualizado);
+      if (produtosService.listarProdutosId(id) != null) {
+            ProdutosDTO produtoAtualizado = produtosService.atualizarProduto(id, produtoatualizado);
+            return ResponseEntity.ok(produtoAtualizado);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("O produto de ID: "+id+" Não foi encontrado.");
     }
 
     // D - DELETE
 
     @DeleteMapping("/deletar/{id}")
-    public void deletarProduto(@PathVariable Long id){
-        produtosService.deletarProduto(id);
+    public ResponseEntity<String> deletarProduto(@PathVariable Long id){
+
+       if (produtosService.listarProdutosId(id) != null) {
+            produtosService.deletarProduto(id);
+            return ResponseEntity.ok("O produto de ID "+id+" foi deletado.");       
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("O produto de ID: "+id+" não foi encontrado.");
+
+        
+       
     }
     
 
